eff2a35e24941957c1e8060bbd9a20cd748ed3e7
ayaatef11 <ayabadrin667@gmail.com>
2025-03-09 09:56:00 +0200

    update blog to use ngfor

--- src/Components/blog/blog.component.ts
+++ src/Components/blog/blog.component.ts
@@ -1,12 +1,31 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-
+  articles = [
+    {
+      image: 'img/images.jpg',
+      title: 'How To Design The Perfect Hero Image: Guide, Practical Tips And Inspiration',
+      description: 'The hero image, a large banner image prominently placed on a web page, generally in the front and center.',
+      link: '#'
+    },
+    {
+      image: 'img/images.jpg',
+      title: 'How To Design The Perfect Hero Image: Guide, Practical Tips And Inspiration',
+      description: 'The hero image, a large banner image prominently placed on a web page, generally in the front and center.',
+      link: '#'
+    },
+    {
+      image: 'img/images.jpg',
+      title: 'How To Design The Perfect Hero Image: Guide, Practical Tips And Inspiration',
+      description: 'The hero image, a large banner image prominently placed on a web page, generally in the front and center.',
+      link: '#'
+    }];
 }
