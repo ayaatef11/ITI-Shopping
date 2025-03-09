e8f916b2f22ca9d564a7e99190a0395df01ec483
ayaatef11 <ayabadrin667@gmail.com>
2025-03-09 09:57:06 +0200

    create article model

---
+++ src/Models/Article.model.ts
@@ -0,0 +1,13 @@
+export class Article{
+  image:string;
+  title:string;
+  description:string;
+  link:string;
+
+  constructor(image:string,title:string,description:string,link:string) {
+    this.image=image;
+    this.title=title;
+    this.description=description;
+    this.link=link;
+  }
+}
